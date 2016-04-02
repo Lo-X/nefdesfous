c3be3636273e63e7844d6bbc6c8ddc3e377652a2
Loïc Boutter <loic.boutter@gmail.com>
2016-04-02 23:23:26 +0200

    Adding slick (carousel jquery plugin) and custom slick theme using FontAwesome

--- js/app.js
+++ js/app.js
@@ -1,5 +1,13 @@
 jQuery(function($) {
     $(document).ready(function() {
+        $('.carousel').slick({
+            autoplay: true,
+            autoplaySpeed: 3000,
+            dots: true,
+            arrows: false,
+            easing: 'swing',
+        });
+
         $('.match-height').matchHeight({
             byRow: true
         });
