18ebf3ba7619e22601408c7575c6295f91025ee9
Loïc Boutter <loic.boutter@gmail.com>
2016-04-02 23:18:55 +0200

    Return to top by scrolling

---
+++ js/app.js
@@ -0,0 +1,17 @@
+jQuery(function($) {
+    $(document).ready(function() {
+        $('body').append('<a href="#" id="return-to-top" title="Retour en haut"><i class="fa fa-arrow-up"></i></a>');
+        $('#return-to-top').on('click', function(event) {
+            event.preventDefault();
+            $('html,body').animate({scrollTop: 0}, 'slow');
+        });
+
+        $(window).scroll(function() {
+            if($(window).scrollTop() === 0) {
+                $('#return-to-top').fadeOut();
+            } else {
+                $('#return-to-top').fadeIn();
+            }
+        });
+    });
+});
