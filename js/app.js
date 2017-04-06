23fbe56570a8213d65a868c68c7302089947c9f6
Loïc Boutter <loic.boutter@gmail.com>
2017-04-06 14:05:58 +0200

    Integration of the event calendar

--- js/app.js
+++ js/app.js
@@ -27,6 +27,28 @@ jQuery(function($) {
             }
         });
 
-        $('#calendar').clndr({});
+        moment.locale('fr');
+        var currentMonth = moment().format('YYYY-MM');
+        var nextMonth    = moment().add('month', 1).format('YYYY-MM');
+        var events = [
+            { date: currentMonth + '-' + '10', date_fr: moment(currentMonth + '-' + '10').format('LL'), title: 'Persian Kitten Auction', description: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Architecto asperiores, corporis dolorem eum ipsa magni molestiae porro quis quod, ratione repellendus vel velit veniam? Praesentium quibusdam sunt vero? Quam, repudiandae.', facebook: 'https://www.facebook.com/events/236119216854385/' },
+            { date: currentMonth + '-' + '19', date_fr: moment(currentMonth + '-' + '19').format('LL'),title: 'Cat Frisbee', description: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Architecto asperiores, corporis dolorem eum ipsa magni molestiae porro quis quod, ratione repellendus vel velit veniam? Praesentium quibusdam sunt vero? Quam, repudiandae.' },
+            { date: currentMonth + '-' + '23', date_fr: moment(currentMonth + '-' + '23').format('LL'),title: 'Kitten Demonstration', description: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Architecto asperiores, corporis dolorem eum ipsa magni molestiae porro quis quod, ratione repellendus vel velit veniam? Praesentium quibusdam sunt vero? Quam, repudiandae.' },
+            { date: nextMonth + '-' + '07',    date_fr: moment(nextMonth + '-' + '07').format('LL'),title: 'Small Cat Photo Session', description: 'Center for Cat Photography' }
+        ];
+        $('#calendar').clndr({
+            template: $('#full-clndr-template').html(),
+            events: events,
+            forceSixRows: true,
+            moment: moment,
+            clickEvents: {
+                click: function (target) {
+                    if (target.events.length == 1 && typeof target.events[0].facebook != 'undefined') {
+                        var win = window.open(target.events[0].facebook, '_blank');
+                        win.focus();
+                    }
+                },
+            },
+        });
     });
 });
