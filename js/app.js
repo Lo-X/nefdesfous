a4bc608d74c5c74d5c94d206a23a796cbcda0a36
Loïc Boutter <loic.boutter@gmail.com>
2017-04-07 14:16:11 +0200

    Adding next events as placeholders and scroll as a one page website

--- js/app.js
+++ js/app.js
@@ -18,6 +18,13 @@ jQuery(function($) {
             event.preventDefault();
             $('html,body').animate({scrollTop: 0}, 'slow');
         });
+        $('#main-nav a').on('click', function(event) {
+            event.preventDefault();
+            var anchor = $(this).attr('href');
+            var target = $(anchor);
+            $('html,body').animate({scrollTop: target.offset().top - 150}, 'slow');
+            location.hash = anchor;
+        });
 
         $(window).scroll(function() {
             if($(window).scrollTop() === 0) {
@@ -28,27 +35,17 @@ jQuery(function($) {
         });
 
         moment.locale('fr');
-        var currentMonth = moment().format('YYYY-MM');
-        var nextMonth    = moment().add('month', 1).format('YYYY-MM');
         var events = [
-            { date: currentMonth + '-' + '10', date_fr: moment(currentMonth + '-' + '10').format('LL'), title: 'Persian Kitten Auction', description: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Architecto asperiores, corporis dolorem eum ipsa magni molestiae porro quis quod, ratione repellendus vel velit veniam? Praesentium quibusdam sunt vero? Quam, repudiandae.', facebook: 'https://www.facebook.com/events/236119216854385/' },
-            { date: currentMonth + '-' + '19', date_fr: moment(currentMonth + '-' + '19').format('LL'),title: 'Cat Frisbee', description: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Architecto asperiores, corporis dolorem eum ipsa magni molestiae porro quis quod, ratione repellendus vel velit veniam? Praesentium quibusdam sunt vero? Quam, repudiandae.' },
-            { date: currentMonth + '-' + '23', date_fr: moment(currentMonth + '-' + '23').format('LL'),title: 'Kitten Demonstration', description: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Architecto asperiores, corporis dolorem eum ipsa magni molestiae porro quis quod, ratione repellendus vel velit veniam? Praesentium quibusdam sunt vero? Quam, repudiandae.' },
-            { date: nextMonth + '-' + '07',    date_fr: moment(nextMonth + '-' + '07').format('LL'),title: 'Small Cat Photo Session', description: 'Center for Cat Photography' }
+            { date: '2017-04-08 20:00:00', date_fr: moment('2017-04-08 20:00:00').format('LLL'), title: 'P V L S Λ R', description: 'Entre voyage interstellaire et ode à l\'humanité, PVLSAR distille à travers un électro-pop teinté de rock une mélodie planante mêlée a des percussions puissantes. C\'est fin 2016 après une année de gestation que le projet prend enfin son envol, développant son propre univers de science-fiction mais surtout son propre style cross-média alliant à la fois lumière son et image.', facebook: 'https://www.facebook.com/events/236119216854385/' },
+            { date: '2017-04-27 20:00:00', date_fr: moment('2017-04-27 20:00:00').format('LLL'),title: 'Livresque (les jeudis de la Nef)', description: 'Au menu de la soirée : les gens d\'ici !<br>Ils parlent deux langues, mangent de la Wurst et des Krumpakieschele, s\'appellent Muller, Fischer ou Weber... Pourquoi ? Le roman de Pascale Hugues "Marthe et Mathilde" - une famille entre France et Allemagne - vous éclairera. Une soirée légèrement bilingue et musicale, mais pas que...' },
+            { date: '2017-04-29 20:00:00', date_fr: moment('2017-04-29 21:00:00').format('LLL'),title: 'Allan Ros', description: 'Furieux ou intimiste, Allan Ros dresse, à travers un jeu de guitare atypique et un métissage de culture, un portrait moderne de la musique folk. Après un premier mini-album, ONCE, sorti en octobre 2014. Allan Ros sort le 7 mai 2016 son premier album : 10 titres qui cassent les codes de la musique folk, alliant l’énergie et la fureur du rock aux sonorités de la guitare acoustique.<br>La Bellevilloise (http://www.labellevilloise.com/allan-ros/)', facebook: 'https://www.facebook.com/AllanRosMusic/' },
+            { date: '2017-05-11 20:00:00', date_fr: moment('2017-05-11 20:00:00').format('LLL'),title: 'Rencontre avec des agriculteurs Burkinabè (les jeudis de la Nef)', description: 'Plus d\'informations à venir. Rencontre, échange, débat organisé par l\'association Mil\'École avec des agriculteurs Burkinabè - Le bocage sahélien', facebook: 'https://www.facebook.com/ongmilecole/' },
         ];
         $('#calendar').clndr({
             template: $('#full-clndr-template').html(),
             events: events,
             forceSixRows: true,
             moment: moment,
-            clickEvents: {
-                click: function (target) {
-                    if (target.events.length == 1 && typeof target.events[0].facebook != 'undefined') {
-                        var win = window.open(target.events[0].facebook, '_blank');
-                        win.focus();
-                    }
-                },
-            },
         });
     });
 });
